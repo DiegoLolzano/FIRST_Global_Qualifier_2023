45f9c4c47b575b13e9a8d48637e05334d596094d
DiegoLolzano <diegolozanor66@gmail.com>
2023-06-20 09:55:28 -0600

    Edited more OpModes for today

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Autos/PracticeChallenges/June20/PlaceholderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Autos/PracticeChallenges/June20/PlaceholderAuto.java
@@ -1,12 +1,47 @@
 package org.firstinspires.ftc.teamcode.OpMode.Autos.PracticeChallenges.June20;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.CerbLib.IMUPathAlgorithm;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain.DriveTrain;
 
 @Autonomous
 public class PlaceholderAuto extends CommandOpMode {
+    DriveTrain m_drive;
+
+    IMUPathAlgorithm pathAlgorithm;
+    FtcDashboard dashboard;
+    TelemetryPacket packet;
+
     @Override
     public void initialize() {
+        m_drive = new DriveTrain(hardwareMap);
 
+        pathAlgorithm = new IMUPathAlgorithm(m_drive);
+
+        dashboard = FtcDashboard.getInstance();
+        packet = new TelemetryPacket();
+        telemetry = new MultipleTelemetry();
+
+        register(m_drive);
+
+        schedule(new SequentialCommandGroup(
+
+        ));
+
+        schedule(new RunCommand(() -> {
+            packet.put("Left inches", DriveConstants.encoderTicksToInches(m_drive.getLeftCurrentPos()));
+            packet.put("Right inches", DriveConstants.encoderTicksToInches(m_drive.getRightCurrentPos()));
+            packet.put("Robot Angle", m_drive.getAngle());
+
+            dashboard.sendTelemetryPacket(packet);
+        }));
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/PracticeChallenges/June20/June20Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/PracticeChallenges/June20/June20Tele.java
@@ -1,12 +1,35 @@
 package org.firstinspires.ftc.teamcode.OpMode.TeleOp.PracticeChallenges.June20;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.RunCommand;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain.DriveTrain;
 
 @TeleOp
 public class June20Tele extends CommandOpMode {
+    DriveTrain m_drive;
     @Override
     public void initialize() {
+        m_drive = new DriveTrain(hardwareMap);
 
+        register(m_drive);
+
+        /*new GamepadButton(new GamepadEx(gamepad1),
+                GamepadKeys.Button.A).whenPressed(null);
+
+        new GamepadButton(new GamepadEx(gamepad1),
+                GamepadKeys.Button.B).whenPressed(null);
+
+        new GamepadButton(new GamepadEx(gamepad1),
+                GamepadKeys.Button.Y).whenPressed(null);
+
+
+        new GamepadButton(new GamepadEx(gamepad1),
+                GamepadKeys.Button.X).whenPressed(null);*/
+
+        schedule(new RunCommand(() -> {
+            //In case of running any telemetry (Driver HUB Dashboard)
+        }));
     }
 }
